def67ba7b5d325c89c2c492fffa1aa4e14511710
iny7 <466924784@qq.com>
2016-07-08 20:36:52 +0800

    ADD: 引入了各种颜色互相转换的函数
    根据滑条的位置,得到HSB中的H

--- js/color-picker.js
+++ js/color-picker.js
@@ -84,8 +84,9 @@
       // options里 关于色带,存left就可以了,内部再计算
       if(offsetX != this.colBandValue){
         this.colBandValue = offsetX;
-        console.log(offsetX)
-        this.colorBtn.css('left', offsetX);  
+        this.colorBtn.css('left', offsetX);
+        var percent = 360 - 360 * offsetX / (this.colorBandWidth - this.colorBtnWidth);
+        console.log(percent)
       }
       
     },
@@ -239,9 +240,72 @@
     picker.init();
     return picker;
   }
+
+  //Color space convertions
+  var hexToRgb = function (hex) {
+    var hex = parseInt(((hex.indexOf('#') > -1) ? hex.substring(1) : hex), 16);
+    return {r: hex >> 16, g: (hex & 0x00FF00) >> 8, b: (hex & 0x0000FF)};
+  };
+  var hexToHsb = function (hex) {
+    return rgbToHsb(hexToRgb(hex));
+  };
+  var rgbToHsb = function (rgb) {
+    var hsb = {h: 0, s: 0, b: 0};
+    var min = Math.min(rgb.r, rgb.g, rgb.b);
+    var max = Math.max(rgb.r, rgb.g, rgb.b);
+    var delta = max - min;
+    hsb.b = max;
+    hsb.s = max != 0 ? 255 * delta / max : 0;
+    if (hsb.s != 0) {
+      if (rgb.r == max) hsb.h = (rgb.g - rgb.b) / delta;
+      else if (rgb.g == max) hsb.h = 2 + (rgb.b - rgb.r) / delta;
+      else hsb.h = 4 + (rgb.r - rgb.g) / delta;
+    } else hsb.h = -1;
+    hsb.h *= 60;
+    if (hsb.h < 0) hsb.h += 360;
+    hsb.s *= 100/255;
+    hsb.b *= 100/255;
+    return hsb;
+  };
+  var hsbToRgb = function (hsb) {
+    var rgb = {};
+    var h = hsb.h;
+    var s = hsb.s*255/100;
+    var v = hsb.b*255/100;
+    if(s == 0) {
+      rgb.r = rgb.g = rgb.b = v;
+    } else {
+      var t1 = v;
+      var t2 = (255-s)*v/255;
+      var t3 = (t1-t2)*(h%60)/60;
+      if(h==360) h = 0;
+      if(h<60) {rgb.r=t1; rgb.b=t2; rgb.g=t2+t3}
+      else if(h<120) {rgb.g=t1; rgb.b=t2; rgb.r=t1-t3}
+      else if(h<180) {rgb.g=t1; rgb.r=t2; rgb.b=t2+t3}
+      else if(h<240) {rgb.b=t1; rgb.r=t2; rgb.g=t1-t3}
+      else if(h<300) {rgb.b=t1; rgb.g=t2; rgb.r=t2+t3}
+      else if(h<360) {rgb.r=t1; rgb.g=t2; rgb.b=t1-t3}
+      else {rgb.r=0; rgb.g=0; rgb.b=0}
+    }
+    return {r:Math.round(rgb.r), g:Math.round(rgb.g), b:Math.round(rgb.b)};
+  };
+  var rgbToHex = function (rgb) {
+    var hex = [
+      rgb.r.toString(16),
+      rgb.g.toString(16),
+      rgb.b.toString(16)
+    ];
+    $.each(hex, function (nr, val) {
+      if (val.length == 1) {
+        hex[nr] = '0' + val;
+      }
+    });
+    return hex.join('');
+  };
+  var hsbToHex = function (hsb) {
+    return rgbToHex(hsbToRgb(hsb));
+  };
   
 }(jQuery))
-
-$(function(){
   
 })
